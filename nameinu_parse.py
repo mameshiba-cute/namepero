ff61f2b7669c982ca77050782b8c0c81e04d8759
hiroya-y <hiroya-y@skytemple.local>
2018-09-02 10:22:18 +0900

    change source

--- nameinu_parse.py
+++ nameinu_parse.py
@@ -4,6 +4,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+""" 初期変数の定義 """
+
 # URLを変数に格納
 req_url = "https://cunni.info/list?sex=woman&age=all&region=kanto"
 
@@ -11,6 +13,22 @@ req_url = "https://cunni.info/list?sex=woman&age=all&region=kanto"
 soup = BeautifulSoup((requests.get(req_url)).content, "html.parser")
 
 # そのうち直近 3postを抽出
-new_post = [soup.find_all("div", class_="card-content black-text")[0],
-            soup.find_all("div", class_="card-content black-text")[1],
-            soup.find_all("div", class_="card-content black-text")[2]]
+new_post = [
+    soup.find_all("div", class_="card-content black-text")[0],
+    soup.find_all("div", class_="card-content black-text")[1],
+    soup.find_all("div", class_="card-content black-text")[2],
+    ]
+
+
+""" 関数定義 """
+
+# bs4.element.Tag -> str。ついでに投稿者情報(改行+余白)の置換
+def __hoge__(mike):
+    o = mike.encode("utf-8").replace('\n                  ', ' ')
+    return o
+
+""" メイン """
+
+for post in new_post:
+    print __hoge__(post.text)
+    print ("------------------------------------------------------------")
